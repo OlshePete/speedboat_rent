033bb0fbad5793db8d9a7ab1986e1247fe818868
OlshePete <olshevskii.pv@gmail.com>
2022-10-04 12:42:05 +0300

    delete history folder

--- .history/src/components/NewOrderPage/ButtonGroupSpot_20220925121119.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ButtonGroupSpot() {
-  return (
-    <div>ButtonGroupSpot</div>
-  )
-}
-
-export default ButtonGroupSpot
--- .history/src/components/NewOrderPage/ResultBlock_20220926120910.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ResultBlock() {
-  return (
-    <div>ResultBlock</div>
-  )
-}
-
-export default ResultBlock
